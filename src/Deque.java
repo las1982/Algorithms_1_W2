0e720aca8ab7379a6026397aa830e27bbecd7a8c
las1982 <lashkov_alex@mail.ru>
2017-04-19 20:04:50 +0300

    some changes

--- src/Deque.java
+++ src/Deque.java
@@ -1,226 +1,229 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import static edu.princeton.cs.algs4.StdOut.print;
-import static edu.princeton.cs.algs4.StdOut.println;
+//import static edu.princeton.cs.algs4.StdOut.println;
 
 public class Deque<Item> implements Iterable<Item> {
-	private Item[] a;
-	private int n;
-	private int lastInd;
-	private int firstInd;
+    private Item[] a;
+    private int n;
+    private int lastInd;
+    private int firstInd;
 
-	public Deque() {
-		a = (Item[]) new Object[3];
-		n = 0;
-		ResetIndeces();
-	}
+    public Deque() {
+        a = (Item[]) new Object[3];
+        n = 0;
+        resetIndeces();
+    }
 
-	private void ResetIndeces() {
-		firstInd = 1;
-		lastInd = 1;
+    private void resetIndeces() {
+        firstInd = 1;
+        lastInd = 1;
 
-	}
+    }
 
-	public boolean isEmpty() {
-		return n == 0;
-	}
+    public boolean isEmpty() {
+        return n == 0;
+    }
 
-	public int size() {
-		return n;
-	}
+    public int size() {
+        return n;
+    }
 
-	private void resize(int capacity) {
-		assert capacity >= n;
-		Item[] temp = (Item[]) new Object[capacity];
-		int newFirst = (capacity - n) / 2;
-		int tempStart = newFirst;
-		for (int i = firstInd; i <= lastInd; i++) {
-			temp[tempStart++] = a[i];
-		}
-		firstInd = newFirst;
-		lastInd = firstInd + n - 1;
-		println("Resized from " + a.length + " to " + capacity);
-		a = temp;
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Item[] temp = (Item[]) new Object[capacity];
+        int newFirst = (capacity - n) / 2;
+        int tempStart = newFirst;
+        for (int i = firstInd; i <= lastInd; i++) {
+            temp[tempStart++] = a[i];
+        }
+        firstInd = newFirst;
+        lastInd = firstInd + n - 1;
+//        StdOut.println("Resized from " + a.length + " to " + capacity);
+        a = temp;
 
-		// a = java.util.Arrays.copyOf(a, capacity);
-	}
+        // a = java.util.Arrays.copyOf(a, capacity);
+    }
 
-	public void addFirst(Item item) {
-		if (item == null)
-			throw new java.lang.NullPointerException("cant add null");
-		if (firstInd == 0 || lastInd == a.length - 1)
-			resize(2 * a.length); // double size of array if necessary
-		if (n == 0) {
-			a[firstInd] = item;
-		} else {
-			a[--firstInd] = item;
-		}
-		n++;
-		println("Added first");
-		println(size() + " of " + a.length);
-		println("firstInd = " + firstInd + ", lastInd = " + lastInd);
-	}
+    public void addFirst(Item item) {
+        if (item == null)
+            throw new java.lang.NullPointerException("cant add null");
+        if (firstInd == 0 || lastInd == a.length - 1)
+            resize(2 * a.length); // double size of array if necessary
+        if (n == 0) {
+            a[firstInd] = item;
+        }
+        else {
+            a[--firstInd] = item;
+        }
+        n++;
+//        StdOut.println("Added first");
+//        StdOut.println(size() + " of " + a.length);
+//        StdOut.println("firstInd = " + firstInd + ", lastInd = " + lastInd);
+    }
 
-	public void addLast(Item item) {
-		if (item == null)
-			throw new java.lang.NullPointerException("cant add null");
-		if (firstInd == 0 || lastInd == a.length - 1)
-			resize(2 * a.length); // double size of array if necessary
-		if (n == 0) {
-			a[lastInd] = item;
-		} else {
-			a[++lastInd] = item;
-		}
-		n++;
-		println("Added last");
-		println(size() + " of " + a.length);
-		println("firstInd = " + firstInd + ", lastInd = " + lastInd);
-	}
+    public void addLast(Item item) {
+        if (item == null)
+            throw new java.lang.NullPointerException("cant add null");
+        if (firstInd == 0 || lastInd == a.length - 1)
+            resize(2 * a.length); // double size of array if necessary
+        if (n == 0) {
+            a[lastInd] = item;
+        }
+        else {
+            a[++lastInd] = item;
+        }
+        n++;
+//        StdOut.println("Added last");
+//        StdOut.println(size() + " of " + a.length);
+//        StdOut.println("firstInd = " + firstInd + ", lastInd = " + lastInd);
+    }
 
-	public Item removeFirst() {
-		if (isEmpty())
-			throw new NoSuchElementException("Stack underflow");
-		Item item = a[firstInd];
-		if (n == 1) {
-			a[firstInd] = null; // to avoid loitering
-			ResetIndeces();
-		} else {
-			a[firstInd++] = null; // to avoid loitering
-		}
-		n--;
-		// shrink size of array if necessary
-		if (n > 0 && n == a.length / 4)
-			resize(a.length / 2);
-		println("Removed first");
-		println(size() + " of " + a.length);
-		println("firstInd = " + firstInd + ", lastInd = " + lastInd);
-		return item;
-	}
+    public Item removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException("Stack underflow");
+        Item item = a[firstInd];
+        if (n == 1) {
+            a[firstInd] = null; // to avoid loitering
+            resetIndeces();
+        }
+        else {
+            a[firstInd++] = null; // to avoid loitering
+        }
+        n--;
+        // shrink size of array if necessary
+        if (n > 0 && n == a.length / 4)
+            resize(a.length / 2);
+//        StdOut.println("Removed first");
+//        StdOut.println(size() + " of " + a.length);
+//        StdOut.println("firstInd = " + firstInd + ", lastInd = " + lastInd);
+        return item;
+    }
 
-	public Item removeLast() {
-		if (isEmpty())
-			throw new NoSuchElementException("Stack is empty");
-		Item item = a[lastInd];
-		if (n == 1) {
-			a[lastInd] = null; // to avoid loitering
-			ResetIndeces();
-		} else {
-			a[lastInd--] = null; // to avoid loitering
-		}
-		n--;
-		// shrink size of array if necessary
-		if (n > 0 && n == a.length / 4)
-			resize(a.length / 2);
-		println("Removed last");
-		println(size() + " of " + a.length);
-		println("firstInd = " + firstInd + ", lastInd = " + lastInd);
-		return item;
-	}
+    public Item removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException("Stack is empty");
+        Item item = a[lastInd];
+        if (n == 1) {
+            a[lastInd] = null; // to avoid loitering
+            resetIndeces();
+        }
+        else {
+            a[lastInd--] = null; // to avoid loitering
+        }
+        n--;
+        // shrink size of array if necessary
+        if (n > 0 && n == a.length / 4)
+            resize(a.length / 2);
+//        StdOut.println("Removed last");
+//        StdOut.println(size() + " of " + a.length);
+//        StdOut.println("firstInd = " + firstInd + ", lastInd = " + lastInd);
+        return item;
+    }
 
-	// return an iterator over items in order from front to end
-	public Iterator<Item> iterator() {
-		return new ArrayIterator();
-	}
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }
 
-	// an iterator, doesn't implement remove() since it's optional
-	private class ArrayIterator implements Iterator<Item> {
-		private int i;
+    // an iterator, doesn't implement remove() since it's optional
+    private class ArrayIterator implements Iterator<Item> {
+        private int i;
 
-		public ArrayIterator() {
-			i = firstInd;
-		}
+        public ArrayIterator() {
+            i = firstInd;
+        }
 
-		public boolean hasNext() {
-			return i <= lastInd && n > 0;
-		}
+        public boolean hasNext() {
+            return i <= lastInd && n > 0;
+        }
 
-		public void remove() {
-			throw new UnsupportedOperationException("Remove operation isn't supported");
-		}
+        public void remove() {
+            throw new UnsupportedOperationException("Remove operation isn't supported");
+        }
 
-		public Item next() {
-			if (!hasNext())
-				throw new NoSuchElementException("There isn't next element");
-			return a[i++];
-		}
+        public Item next() {
+            if (!hasNext())
+                throw new NoSuchElementException("There isn't next element");
+            return a[i++];
+        }
 
-		public String toString() {
-			String str = "";
-			while (hasNext()) {
-				str = str + next() + " ";
-			}
-			return str;
-		}
-	}
+//        public String toString() {
+//            String str = "";
+//            while (hasNext()) {
+//                str = str + next() + " ";
+//                str = str.concat(next()).concat(" ");
+//            }
+//            return str;
+//        }
+    }
 
-	// unit testing (optional)
-	public static void main(String[] args) {
-		Deque d = new Deque();
-		println(d.size() + " of " + d.a.length);
-		println("firstInd = " + d.firstInd + ", lastInd = " + d.lastInd);
-
-		d.addFirst(1);
-		println(d.iterator().toString());
-
-		d.addFirst(2);
-		println(d.iterator().toString());
-
-		d.removeLast();
-		println(d.iterator().toString());
-
-		d.removeLast();
-		println(d.iterator().toString());
-		
-		d.addFirst(3);
-		println(d.iterator().toString());
-
-		d.removeFirst();
-		println(d.iterator().toString());
-
-		d.addFirst(4);
-		println(d.iterator().toString());
-
-		d.addFirst(5);
-		println(d.iterator().toString());
-
-		d.addFirst(6);
-		println(d.iterator().toString());
-
-		d.addFirst(7);
-		println(d.iterator().toString());
-
-		d.removeLast();
-		println(d.iterator().toString());
-
-		d.removeLast();
-		println(d.iterator().toString());
-
-		d.removeLast();
-		println(d.iterator().toString());
-		
-		// ************************************************
-		d.addLast(8);
-		println(d.iterator().toString());
-		
-		d.addLast(9);
-		println(d.iterator().toString());
-		
-		d.addLast(10);
-		println(d.iterator().toString());
-		
-		d.removeFirst();
-		println(d.iterator().toString());
-		
-		d.removeFirst();
-		println(d.iterator().toString());
-		
-		d.removeFirst();
-		println(d.iterator().toString());
-		
-		d.removeFirst();
-		println(d.iterator().toString());
-		
-		
-
-	}
+    // unit testing (optional)
+    public static void main(String[] args) {
+//        Deque d = new Deque();
+//        println(d.size() + " of " + d.a.length);
+//        println("firstInd = " + d.firstInd + ", lastInd = " + d.lastInd);
+//
+//        d.addFirst(1);
+//        println(d.iterator().toString());
+//
+//        d.addFirst(2);
+//        println(d.iterator().toString());
+//
+//        d.removeLast();
+//        println(d.iterator().toString());
+//
+//        d.removeLast();
+//        println(d.iterator().toString());
+//
+//        d.addFirst(3);
+//        println(d.iterator().toString());
+//
+//        d.removeFirst();
+//        println(d.iterator().toString());
+//
+//        d.addFirst(4);
+//        println(d.iterator().toString());
+//
+//        d.addFirst(5);
+//        println(d.iterator().toString());
+//
+//        d.addFirst(6);
+//        println(d.iterator().toString());
+//
+//        d.addFirst(7);
+//        println(d.iterator().toString());
+//
+//        d.removeLast();
+//        println(d.iterator().toString());
+//
+//        d.removeLast();
+//        println(d.iterator().toString());
+//
+//        d.removeLast();
+//        println(d.iterator().toString());
+//
+//        // ************************************************
+//        d.addLast(8);
+//        println(d.iterator().toString());
+//
+//        d.addLast(9);
+//        println(d.iterator().toString());
+//
+//        d.addLast(10);
+//        println(d.iterator().toString());
+//
+//        d.removeFirst();
+//        println(d.iterator().toString());
+//
+//        d.removeFirst();
+//        println(d.iterator().toString());
+//
+//        d.removeFirst();
+//        println(d.iterator().toString());
+//
+//        d.removeFirst();
+//        println(d.iterator().toString());
+//
+//
+    }
 }
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,177 +1,178 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 import edu.princeton.cs.algs4.StdRandom;
-import static edu.princeton.cs.algs4.StdOut.print;
-import static edu.princeton.cs.algs4.StdOut.println;
+//import static edu.princeton.cs.algs4.StdOut.println;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-	private Item[] a; // array of items
-	private int n; // number of elements on stack
+    private Item[] a; // array of items
+    private int n; // number of elements on stack
 
-	// construct an empty randomized queue
-	public RandomizedQueue() {
-		a = (Item[]) new Object[2];
-		n = 0;
-	}
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        a = (Item[]) new Object[2];
+        n = 0;
+    }
 
-	// is the queue empty?
-	public boolean isEmpty() {
-		return n == 0;
-	}
+    // is the queue empty?
+    public boolean isEmpty() {
+        return n == 0;
+    }
 
-	// return the number of items on the queue
-	public int size() {
-		return n;
-	}
+    // return the number of items on the queue
+    public int size() {
+        return n;
+    }
 
-	// resize the underlying array holding the elements
-	private void resize(int capacity) {
-		assert capacity >= n;
+    // resize the underlying array holding the elements
+    private void resize(int capacity) {
+        assert capacity >= n;
 
-		// textbook implementation
-		Item[] temp = (Item[]) new Object[capacity];
-		for (int i = 0; i < n; i++) {
-			temp[i] = a[i];
-		}
-		a = temp;
+        // textbook implementation
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
 
-		// alternative implementation
-		// a = java.util.Arrays.copyOf(a, capacity);
-	}
+        // alternative implementation
+        // a = java.util.Arrays.copyOf(a, capacity);
+    }
 
-	// add the item
-	public void enqueue(Item item) {
-		if (item == null)
-			throw new java.lang.NullPointerException("Can't add null");
-		if (n == a.length)
-			resize(2 * a.length); // double size of array if necessary
-		a[n++] = item;
-	}
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null)
+            throw new java.lang.NullPointerException("Can't add null");
+        if (n == a.length)
+            resize(2 * a.length); // double size of array if necessary
+        a[n++] = item;
+    }
 
-	private void shuffle() {
-		Item[] temp = (Item[]) new Object[n];
-		for (int i = 0; i < n; i++) {
-			temp[i] = a[i];
-		}
-		StdRandom.shuffle(temp);
-		for (int i = 0; i < n; i++) {
-			a[i] = temp[i];
-		}
-	}
+    private void shuffle() {
+        Item[] temp = (Item[]) new Object[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = a[i];
+        }
+        StdRandom.shuffle(temp);
+        for (int i = 0; i < n; i++) {
+            a[i] = temp[i];
+        }
+    }
 
-	// remove and return a random item
-	public Item dequeue() {
-		if (isEmpty())
-			throw new NoSuchElementException("Stack is empty");
-		shuffle();
-		Item item = a[n - 1];
-		a[n - 1] = null; // to avoid loitering
-		n--;
-		// shrink size of array if necessary
-		if (n > 0 && n == a.length / 4)
-			resize(a.length / 2);
-		return item;
-	}
+    // remove and return a random item
+    public Item dequeue() {
+        if (isEmpty())
+            throw new NoSuchElementException("Stack is empty");
+        shuffle();
+        Item item = a[n - 1];
+        a[n - 1] = null; // to avoid loitering
+        n--;
+        // shrink size of array if necessary
+        if (n > 0 && n == a.length / 4)
+            resize(a.length / 2);
+        return item;
+    }
 
-	// return (but do not remove) a random item
-	public Item sample() {
-		if (isEmpty())
-			throw new NoSuchElementException("Stack is empty");
-		shuffle();
-		Item item = a[n - 1];
-		return item;
-	}
+    // return (but do not remove) a random item
+    public Item sample() {
+        if (isEmpty())
+            throw new NoSuchElementException("Stack is empty");
+        shuffle();
+        Item item = a[n - 1];
+        return item;
+    }
 
-	public Iterator<Item> iterator() {
-		return new RandomeArrayIterator();
-	}
+    public Iterator<Item> iterator() {
+        return new RandomeArrayIterator();
+    }
 
-	// an iterator, doesn't implement remove() since it's optional
-	private class RandomeArrayIterator implements Iterator<Item> {
-		private int i;
+    // an iterator, doesn't implement remove() since it's optional
+    private class RandomeArrayIterator implements Iterator<Item> {
+        private int i;
+        Item[] temp;
 
-		public RandomeArrayIterator() {
-			shuffle();
-			i = 0;
-		}
+        public RandomeArrayIterator() {
+            shuffle();
+            temp = (Item[]) new Object[a.length];
+            temp = a;
+            i = 0;
+        }
 
-		public boolean hasNext() {
-			return i < n;
-		}
+        public boolean hasNext() {
+            return i < n;
+        }
 
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
-		public Item next() {
-			if (!hasNext())
-				throw new NoSuchElementException();
-			return a[i++];
-		}
+        public Item next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            return temp[i++];
+        }
 
-		public String toString() {
-			String str = "";
-			while (hasNext()) {
-				str = str + next() + " ";
-			}
-			return str;
-		}
-	}
+//        public String toString() {
+//            String str = "";
+//            while (hasNext()) {
+//                str = str + next() + " ";
+//            }
+//            return str;
+//        }
+    }
 
-	// unit testing (optional)
-	public static void main(String[] args) {
-		RandomizedQueue rq = new RandomizedQueue();
-		rq.enqueue(1);
-		println(rq.iterator().toString());
-
-		rq.enqueue(2);
-		println(rq.iterator().toString());
-
-		rq.enqueue(3);
-		println(rq.iterator().toString());
-
-		rq.enqueue(4);
-		println(rq.iterator().toString());
-
-		rq.enqueue(5);
-		println(rq.iterator().toString());
-
-		rq.enqueue(6);
-		println(rq.iterator().toString());
-
-		rq.enqueue(7);
-		println(rq.iterator().toString());
-
-		rq.enqueue(8);
-		println(rq.iterator().toString());
-
-		rq.enqueue(9);
-		println(rq.iterator().toString());
-
-		rq.enqueue(10);
-		println(rq.iterator().toString());
-		// *****************************************************
-
-		rq.dequeue();
-		println(rq.iterator().toString());
-
-		rq.dequeue();
-		println(rq.iterator().toString());
-
-		rq.dequeue();
-		println(rq.iterator().toString());
-
-		println(rq.sample());
-		println(rq.iterator().toString());
-
-		println(rq.sample());
-		println(rq.iterator().toString());
-
-		println(rq.sample());
-		println(rq.iterator().toString());
-
-		println(rq.sample());
-		println(rq.iterator().toString());
-	}
+    // unit testing (optional)
+    public static void main(String[] args) {
+//        RandomizedQueue rq = new RandomizedQueue();
+//        rq.enqueue(1);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(2);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(3);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(4);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(5);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(6);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(7);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(8);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(9);
+//        println(rq.iterator().toString());
+//
+//        rq.enqueue(10);
+//        println(rq.iterator().toString());
+//        // *****************************************************
+//
+//        rq.dequeue();
+//        println(rq.iterator().toString());
+//
+//        rq.dequeue();
+//        println(rq.iterator().toString());
+//
+//        rq.dequeue();
+//        println(rq.iterator().toString());
+//
+//        println(rq.sample());
+//        println(rq.iterator().toString());
+//
+//        println(rq.sample());
+//        println(rq.iterator().toString());
+//
+//        println(rq.sample());
+//        println(rq.iterator().toString());
+//
+//        println(rq.sample());
+//        println(rq.iterator().toString());
+    }
 }
