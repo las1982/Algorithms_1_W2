3e2c732108633548f971f17cd388c1cbc9883320
las1982 <lashkov_alex@mail.ru>
2017-04-20 08:11:08 +0300

    some changes

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,14 +1,20 @@
 import edu.princeton.cs.algs4.StdIn;
+
+import static edu.princeton.cs.algs4.StdOut.print;
 import static edu.princeton.cs.algs4.StdOut.println;
 
 public class Permutation {
 	public static void main(String[] args){
 	   int k = Integer.parseInt(args[0]);
 	   RandomizedQueue rq = new RandomizedQueue();
-	   //StdIn.readString();
-	   while (StdIn.hasNextLine()){
-		   rq.enqueue(StdIn.readString());
-	   }
+//	   println(StdIn.readString());
+//	   println(StdIn.readString());
+       for (int i = 1; i <= k; i++){
+           rq.enqueue(StdIn.readString());
+       }
+//       while (StdIn.hasNextLine()){
+//	   rq.enqueue(StdIn.readString());
+//	   }
 	   for (int i = 1; i <= k; i++){
 		   println(rq.dequeue());
 	   }
